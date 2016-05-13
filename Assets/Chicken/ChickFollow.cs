921acfbd583d2344f805684ebcd735c0608fd63f
Greg Weil <gregweil@pentavera.com>
2016-05-13 16:31:07 -0400

    Chicks that follow you around

---
+++ Assets/Chicken/ChickFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickFollow : MonoBehaviour {
+
+    public ChickenPathRecord target = null;
+    public float distance = 1f;
+
+    Rigidbody2D body = null;
+
+    Vector2 speed = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Called every physics step
+    void FixedUpdate() {
+        Vector2 position = target.pathPosition(distance);
+        body.MovePosition(Vector2.SmoothDamp(transform.position, position, ref speed, 0.05f));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
