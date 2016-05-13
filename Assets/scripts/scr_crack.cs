1b47127578884549f4df1cb0eeb3f1d04b1f874a
Greg Weil <gregweil@pentavera.com>
2016-05-13 18:39:09 -0400

    Added cracks which stun the player when a chick touches them

---
+++ Assets/scripts/scr_crack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_crack : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) {
+        if (!col.CompareTag("Follower")) return;
+        var player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<ChickenMovement>().stun(0.5f);
+    }
+}
