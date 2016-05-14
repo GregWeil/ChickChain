aef0424c2450ce06f66a37de62dc1a2bfe142baf
Greg Weil <gregweil@pentavera.com>
2016-05-13 22:37:41 -0400

    Cars kill the chicks

---
+++ Assets/Chicken/ChickDeath.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickDeath : MonoBehaviour {
+
+    public GameObject effectPrefab = null;
+
+	void Kill () {
+        var effect = Instantiate(effectPrefab);
+        effect.name = effectPrefab.name;
+        effect.transform.position = transform.position;
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
+}
