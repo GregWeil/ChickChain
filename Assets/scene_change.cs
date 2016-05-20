f165337883047456235c0708faf1b1756b58c052
devin <dmanhilpert@yahoo.com>
2016-05-20 03:00:58 -0400

    transition working, ready to build

---
+++ Assets/scene_change.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class scene_change : MonoBehaviour {
+
+    public float timer = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer < 0f)
+        {
+            SceneManager.LoadScene("scene_road");
+        }
+	}
+}
