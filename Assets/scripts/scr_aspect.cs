80b2cf4f16db7ba5e3a294d7d4cc5ce1bc16f1d2
Greg Weil <gregweil@pentavera.com>
2016-05-21 17:39:38 -0400

    Force the aspect ratio using black bars rather than resolution options

---
+++ Assets/scripts/scr_aspect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_aspect : MonoBehaviour {
+
+    float aspect = (16f / 9f);
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var scale = new Vector2((Screen.height * aspect) / Screen.width,
+                                (Screen.width / aspect) / Screen.height);
+        var view = Vector2.Min(scale, Vector2.one);
+        cam.rect = new Rect((Vector2.one - view) / 2f, view);
+    }
+}
